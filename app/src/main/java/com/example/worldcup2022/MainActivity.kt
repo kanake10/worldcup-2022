53725ae4610ee12c6b0c67709dc00b5539a28752
kanake10 <10kanake@gmail.com>
2022-11-11 00:24:35 +0300

    commit

--- app/src/main/java/com/example/worldcup2022/MainActivity.kt
+++ app/src/main/java/com/example/worldcup2022/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.ui.Alignment
 import androidx.compose.runtime.Composable
 import coil.compose.rememberImagePainter
@@ -17,17 +16,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.worldcup2022.core.DataState
-import com.example.worldcup2022.core.MainViewModel
+import com.example.worldcup2022.presentation.state.DataState
+import com.example.worldcup2022.presentation.viewmodel.WorldCupViewModel
 import com.example.worldcup2022.models.Countries
+import com.example.worldcup2022.presentation.screen.HomeScreen
+import com.example.worldcup2022.presentation.screen.ShowCountriesLazyList
 import com.example.worldcup2022.presentation.ui.theme.WorldCup2022Theme
-import dagger.hilt.android.AndroidEntryPoint
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: WorldCupViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,98 +34,14 @@ class MainActivity : ComponentActivity() {
                 Column {
                     TopAppBar(
                         title = {
-                            Text(text = "Food Categories")
+                            Text(text = "World Cup 2022")
                         },
-                        backgroundColor = Color.Black,
-                        contentColor = Color.White
-
+                        backgroundColor = Color.White,
+                        contentColor = Color.Black
                     )
-
-                    SetData(viewModel)
-
+                    HomeScreen(viewModel)
                 }
             }
         }
     }
-
-    @Composable
-    fun SetData(viewModel: MainViewModel) {
-        when (val result = viewModel.response.value) {
-            is DataState.Loading -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
-            }
-            is DataState.Success -> {
-                ShowLazyList(result.data)
-            }
-            is DataState.Failure -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = result.message,
-                        fontSize = MaterialTheme.typography.h5.fontSize,
-                    )
-                }
-            }
-            else -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    Text(
-                        text = "Error Fetching data",
-                        fontSize = MaterialTheme.typography.h5.fontSize,
-                    )
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun ShowLazyList(foods: MutableList<Countries>) {
-        LazyColumn {
-            items(foods) { food ->
-                CardItem(food)
-            }
-        }
-    }
-
-    @Composable
-    fun CardItem(food: Countries) {
-        Card(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .padding(10.dp)
-        ) {
-
-            Box(modifier = Modifier.fillMaxSize()) {
-                Image(
-                    painter = rememberImagePainter(food.imageUrl),
-                    modifier = Modifier.fillMaxSize(),
-                    contentDescription = "My content description",
-                    contentScale = ContentScale.FillWidth
-                )
-
-                Text(
-                    text = food.name!!,
-                    fontSize = MaterialTheme.typography.h5.fontSize,
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .fillMaxWidth(),
-                     //   .background(gray_fade),
-                    textAlign = TextAlign.Center,
-                    color = Color.White
-                )
-            }
-
-        }
-    }
-
 }
--- app/src/main/java/com/example/worldcup2022/core/hiltapp/WorldCup.kt
+++
@@ -1,7 +0,0 @@
-package com.example.worldcup2022.core.hiltapp
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class WorldCup : Application()
